8bb65bf0dc7449cb2a95cc7e18b63320418e0f42
Godand <aezeigwe2003@gmail.com>
2022-08-19 23:42:59 +0000

    missing argguements

--- contracts/Istuckchild.sol
+++ contracts/Istuckchild.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// solhint-disable-next-line
 pragma solidity ^0.8.7;
 
 interface Istuckmaxchild {
     
-    function initialize(uint _price, uint _valueBack, address _mod, address _dev)external;
+    function initialize(uint _price, uint _valueBack, address _mod, address _dev,address _sub)external;
 }
