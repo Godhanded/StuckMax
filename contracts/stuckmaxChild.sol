f61665b1658ecb6a314914c65499c8fc8f78b66d
Godand <aezeigwe2003@gmail.com>
2022-08-30 23:17:59 +0000

    refactor

--- contracts/stuckmaxChild.sol
+++ contracts/stuckmaxChild.sol
@@ -6,8 +6,8 @@
 ██║   ██║██║   ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
 ╚██████╔╝╚██████╔╝██████╔╝██║  ██║██║ ╚████║██████╔╝
  ╚═════╝  ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ 
-*/ 
-                                                  
+*/
+
 // solhint-disable-next-line
 pragma solidity ^0.8.7;
 
@@ -15,105 +15,95 @@ import "./Isubscription.sol";
 
 error invalidAmount();
 error invalidUser();
-error failed(string);
+error only_factory();
+error failed();
 
-contract MetastuckMoviesChild{
+contract MetastuckMoviesChild {
+    uint256 public moviePrice;
+    uint256 public netValue;
+    uint256 public comunityFunds;
+    uint256 public modFunds;
+    uint256 public valuebackpercent;
+    uint256 public fees;
 
+    address public moderator;
+    address public dev;
+    address public factory;
+    address public sub;
 
-    uint moviePrice;
-    uint netValue;
-    uint comunityFunds;
-    uint modFunds;
-    uint valuebackpercent;
-    uint fees;
+    mapping(address => uint256) private deadline;
 
-    address moderator;
-    address dev;
-    address factory;
-    address sub;
-
-    mapping(address=>uint) private deadline;
-
-
-    
-    modifier onlyFactory {
-        if (msg.sender== factory)
-        {
+    modifier onlyFactory() {
+        if (msg.sender == factory) {
             _;
-        }else{
-            revert failed("only factory");
+        } else {
+            revert only_factory();
         }
     }
 
-    constructor()
-    {
-        factory=msg.sender;
+    constructor() {
+        factory = msg.sender;
     }
 
-
-    receive() external payable
-    {
-        uint amount= msg.value;
-        if ((amount == moviePrice) && (block.timestamp>deadline[msg.sender])) 
-        {
-            uint stuckFee= (amount * 5)/100;
-            amount-=stuckFee;
-            netValue+=amount;
-            comunityFunds+=amount*(valuebackpercent/100);
-            modFunds+=(amount-comunityFunds);
-            fees+=stuckFee;
-            deadline[msg.sender]= block.timestamp + 1 days;
-        }else{
+    receive() external payable {
+        uint256 amount = msg.value;
+        if (
+            (amount == moviePrice) && (block.timestamp > deadline[msg.sender])
+        ) {
+            uint256 stuckFee = (amount * 5) / 100;
+            amount -= stuckFee;
+            netValue += amount;
+            comunityFunds += amount * (valuebackpercent / 100);
+            modFunds += (amount - comunityFunds);
+            fees += stuckFee;
+            deadline[msg.sender] = block.timestamp + 1 days;
+        } else {
             revert invalidAmount();
         }
     }
 
-    function pullfunds()external 
-    {
-        if(msg.sender==moderator)
-        {
-            uint fee= fees;
-            uint moderatorValue=modFunds;
-            fees-=fee;
-            modFunds-=moderatorValue;
-            netValue-=(moderatorValue);
-            (bool sentmod, )= payable(moderator).call{value:moderatorValue}("");
-            (bool sentStuck, )= payable(dev).call{value:fee}("");
-            require((sentmod && sentStuck),"transaction failed");
-
-        }else{
+    function pullfunds() external {
+        if (msg.sender == moderator) {
+            uint256 fee = fees;
+            uint256 moderatorValue = modFunds;
+            fees -= fee;
+            modFunds -= moderatorValue;
+            netValue -= (moderatorValue);
+            (bool sentMod, ) = payable(moderator).call{value: moderatorValue}("");
+            (bool sentStuck, ) = payable(dev).call{value: fee}("");
+            if (!(sentMod && sentStuck)) revert failed();
+            //require((sentmod && sentStuck),"transaction failed");
+        } else {
             revert invalidUser();
         }
     }
 
-    function hasAccess(address _addr) external view returns(bool)
-    {
-        if(deadline[msg.sender]>=block.timestamp)
-        {
+    function hasAccess(address _addr) external view returns (bool) {
+        if (
+            (deadline[msg.sender] >= block.timestamp) ||
+            (Isubscription(sub).validTill(_addr) >= block.timestamp)
+        ) {
             return true;
-        }else if(Isubscription(sub).validTill(_addr)>=block.timestamp){
-            return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    function getTimeLeft(address addr) external view returns(uint)
-    {
+    function getTimeLeft(address addr) external view returns (uint256) {
         return deadline[addr];
     }
 
-
-    function initialize(uint _price, uint _valueBack, address _mod, address _dev,address _sub)external onlyFactory
-    {
-        moviePrice=_price;
-        valuebackpercent=_valueBack;
-        moderator=_mod;
-        dev=_dev;
-        sub=_sub;
+    function initialize(
+        uint256 _price,
+        uint256 _valueBack,
+        address _mod,
+        address _dev,
+        address _sub
+    ) external onlyFactory {
+        moviePrice = _price;
+        valuebackpercent = _valueBack;
+        moderator = _mod;
+        dev = _dev;
+        sub = _sub;
     }
-
-
-
-   
 }
